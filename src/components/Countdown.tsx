1800b6891fd5208cedc02a568716469e3bf20951
x0n4d0 <romagnolimauricio@gmail.com>
2021-03-29 16:34:36 -0300

    feat: create Countdown button

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,17 +1,52 @@
+import { setPriority } from 'os';
+import { useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountdown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    const ONE_SECOND = 1000;
+
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, ONE_SECOND);
+    }
+  }, [active, time]);
+
   return (
-    <div className={styles.countdownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+
+      <button 
+        type='button' 
+        className={styles.countdownButton}
+        onClick={startCountdown}
+      >
+        Iniciar um ciclo
+      </button>
     </div>
   );
 }
