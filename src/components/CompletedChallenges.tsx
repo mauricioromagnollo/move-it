f8f215446264a7f8e3f5c2d41c14aba00c3a853b
x0n4d0 <romagnolimauricio@gmail.com>
2021-03-29 15:55:25 -0300

    feat: add completedChallenges css module

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,6 +1,8 @@
+import styles from '../styles/components/CompletedChallenges.module.css';
+
 export function CompletedChallenges() {
   return (
-    <div>
+    <div className={styles.completedChallengesContainer}>
       <span>Desafios completos</span>
       <span>5</span>
     </div>
