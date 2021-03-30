9595154e7eb1947aa41d9325fc620b4fc75e1eff
x0n4d0 <romagnolimauricio@gmail.com>
2021-03-30 13:55:21 -0300

    feat: add countdown to page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useContext } from 'react';
 
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
@@ -6,13 +7,25 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { ChallengeBox } from "../components/ChallengeBox";
 
+import { CountdownContext } from '../contexts/CountdownContext';
+
+import { serializeFor2Digt } from '../helpers/serializeFor2Digt';
+
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
+  const { isActive, minutes, seconds } = useContext(CountdownContext);
+
+  const [minuteLeft, minuteRight] = serializeFor2Digt(minutes);
+  const [secondLeft, secondRight] = serializeFor2Digt(seconds);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Início | move.it</title>
+        {isActive 
+          ? <title>{minuteLeft}{minuteRight}:{secondLeft}{secondRight} | move.it</title>
+          : <title>Início | move.it</title> 
+        }
       </Head>
       
       <ExperienceBar />
