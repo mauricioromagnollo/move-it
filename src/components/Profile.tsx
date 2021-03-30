7b4046c27d88c531e4202747359d02809922bcbd
x0n4d0 <romagnolimauricio@gmail.com>
2021-03-30 11:15:03 -0300

    feat: change static level 1 to level context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/x0n4d0.png" alt="Maurício Romagnoli"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Maurício Romagnoli</strong>
         <p>
           <img src="icons/level.svg" alt="Level Icon"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
